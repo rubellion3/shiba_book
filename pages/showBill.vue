78a5a1a609e5fc1c9554eee292fd1303bec90390
Pannawat Choosang <pannawat.chs@gmail.com>
2021-01-21 19:47:21 +0700

    update show bill

---
+++ pages/showBill.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <create-bill
+      :changes="changes.changes"
+      :total-sum="changes.totalSum"
+      :discount="changes.discount"
+      :carts="carts"
+    />
+  </div>
+</template>
+
+<script>
+import CreateBill from "~/components/createBill";
+export default {
+  components: { CreateBill },
+  computed: {
+    changes() {
+      return this.$store.state.calData;
+    },
+    carts() {
+      return this.$store.state.carts;
+    }
+  }
+};
+</script>
+
+<style></style>
