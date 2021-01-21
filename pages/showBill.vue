a25458ec46b43566dc4a12141a6d313ee75fac6f
Pannawat Choosang <pannawat.chs@gmail.com>
2021-01-21 20:57:13 +0700

    update

--- pages/showBill.vue
+++ pages/showBill.vue
@@ -1,18 +1,44 @@
 <template>
-  <div>
-    <CreateBill
-      :changes="changes.changes"
-      :total-sum="changes.totalSum"
-      :discount="changes.discount"
-      :carts="carts"
-    />
+  <div class="container">
+    <div class="font-black text-center text-green-500 text-6xl">
+      The Payment is successfully
+    </div>
+    <table class="table">
+      <thead>
+        <th>Title</th>
+        <th>Quantity</th>
+        <th>Price</th>
+      </thead>
+      <tbody>
+        <tr v-for="cart in carts" :key="cart.id">
+          <td class="text-lg">
+            {{ cart.title }}
+          </td>
+          <td class="text-lg">{{ "x" + cart.qul }}</td>
+          <td class="text-lg">{{ cart.price * cart.qul }}</td>
+        </tr>
+        <tr>
+          <td></td>
+          <th>Discount</th>
+          <td class="text-lg">{{ changes.discount || 0 }}</td>
+        </tr>
+        <tr>
+          <td></td>
+          <th>Net</th>
+          <td class="text-lg">{{ changes.totalSum - changes.discount || 0 }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="container text-center text-2xl">
+      {{ "Changes is " + changes.changes }}
+    </div>
   </div>
 </template>
 
 <script>
-import CreateBill from "../components/createBill";
+// import CreateBill from "../components/createBill";
 export default {
-  components: { CreateBill },
+  // components: { CreateBill },
   computed: {
     changes() {
       return this.$store.state.calData;
